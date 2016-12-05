156eb6909d6a67c4a533f072a5331ca2b3b56118
nimtronican <NIMTRONICAN@GMAIL.COM>
2016-12-05 20:49:44 +0530

    Update mp-products.php
    Updated with New Design

--- mp-products.php
+++ mp-products.php
@@ -1,15 +1,27 @@
 <!DOCTYPE html>
 <html lang="en-US">
-
 <head>
 <meta charset="UTF-8">
+<title>Marketplace PDP List Finder</title>
+<style>
+body{font-family:Arial,Verdana,san-serif; background:#E3E3E3;font-size:12px;}
+input[type="text"],label,select{height:23px;font-size:14px;margin-top:10px;padding:3px;}
+select{height:28px;}
+h4{color:#B00002}
+button{width:200px;font-size:14px; background:#188500; color:#FFFFFF; padding:10px; margin-top:10px;font-weight:bold; border:none; border:1px solid #00480A;border-radius:3px; cursor:pointer;}
+table{width:100%;}
+table,td,tr{background:#FFFFFF;border-collapse:collapse;text-align:left;}
+td{padding:10px;}
+</style>
 </head>
-<body>
-<div id="inputdata">
+<body style="text-align:center;">
+<h1>Find Marketplace PDP list under a category</h1>
+<h4>Note: Only the Search URL's which contains "category[]=" is valid to use</h4>
+<div id="inputdata" style="width:80%; padding:20px 10%; text-align:center; border:1px solid #CCC;background:#FFF9F9;font-size:14px;">
 <form id="urlparameters">
-<label>Enter the Search URL</label>
-<input id="parameters" type="text" value="" /><br>
-<label>Select country</label>
+<label>Enter the Search URL</label><br>
+<input id="parameters" type="text" value="" style="width:100%;" /><br><br>
+<label>Select country</label><br>
 <select id="country">
 <option value="0">Select Country</option>
 <option value="en-in">India</option>
@@ -21,13 +33,17 @@
 <option value="en-th">Thailand</option>
 <option value="en-vn">Vietnam</option>
 <option value="en-ph">Phillipines</option>
+<option value="en-za">South Africa</option>
+<option value="en-ae">UAE</option>
+
 </select><br>
-<button id="findproducts">Find Product List</button>
+<button id="findproducts">Find PDP List</button>
 </form>
 </div>
-<div id="contentdata">
 
+<div id="contentdata" style="width:80%; padding:20px 10%; text-align:center; border:1px solid #CCC;background: #BBF0FF;font-size:14px;">
 </div>
+
 </body>
 <!-- Additional JS Controllers -->
 <script type="text/javascript" src="./js/jquery-3.1.1.min.js"></script>
@@ -38,11 +54,11 @@ $(document).ready(function(){
 		urlval = urlval[1];
 		var cntry = $("#country").val();
 		if(urlval !="" && cntry != 0){
-			var geturl = "http://diver.mybluemix.net/marketplace/api/search/v2/api_search?"+urlval+"&locale="+cntry;
-			alert(geturl);
+			var geturl = "//diver.mybluemix.net/marketplace/api/search/v2/api_search?"+urlval+"&locale="+cntry;
+			//alert(geturl);
 			$.get( geturl, function( data ) {
 			  //alert(data["results"]["items"][1]["doc"]["contact"].toSource());
-				var fulldata = '<table border="1">';
+				var fulldata = '<table id="datatbl" border="1" cellpadding="0" cellspacing="0">';
 				var maincount = data["results"]["total"];
 				for(var i=0;i<maincount;i++)
 				{
@@ -53,8 +69,8 @@ $(document).ready(function(){
 				fulldata += '</table>';
 				$("#contentdata").html(fulldata);
 			  
-			  $( "#contentdata" ).prepend( data["results"]["total"]);
-			  alert( "Load was performed." );
+			  $( "#contentdata" ).prepend("<h2>Total number of PDP's: "+data["results"]["total"]+"</h2>");
+			  //alert( "Load was performed." );
 			});
 			return false;
 		}else{
