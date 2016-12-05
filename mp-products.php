14a0b757347743a500f47f6d7049d54238036d11
nimtronican <NIMTRONICAN@GMAIL.COM>
2016-12-05 21:14:22 +0530

    Update mp-products.php

--- mp-products.php
+++ mp-products.php
@@ -6,9 +6,11 @@
 <style>
 body{font-family:Arial,Verdana,san-serif; background:#E3E3E3;font-size:12px;}
 input[type="text"],label,select{height:23px;font-size:14px;margin-top:10px;padding:3px;}
+label{font-weight:bold;}
 select{height:28px;}
 h4{color:#B00002}
-button{width:200px;font-size:14px; background:#188500; color:#FFFFFF; padding:10px; margin-top:10px;font-weight:bold; border:none; border:1px solid #00480A;border-radius:3px; cursor:pointer;}
+button,input[type="button"]{width:200px;font-size:14px; background:#188500; color:#FFFFFF; padding:10px; margin-top:10px;font-weight:bold; border:none; border:1px solid #00480A;border-radius:3px;cursor:pointer;}
+input[type="button"]{ position:absolute; top:10px; right:60px;background-color:#00828C;}
 table{width:100%;}
 table,td,tr{background:#FFFFFF;border-collapse:collapse;text-align:left;}
 td{padding:10px;}
@@ -19,7 +21,7 @@ td{padding:10px;}
 <h4>Note: Only the Search URL's which contains "category[]=" is valid to use</h4>
 <div id="inputdata" style="width:80%; padding:20px 10%; text-align:center; border:1px solid #CCC;background:#FFF9F9;font-size:14px;">
 <form id="urlparameters">
-<label>Enter the Search URL</label><br>
+<label>Enter the Category Search URL like below:<br></label><br>Example: https://www.ibm.com/marketplace/search/sg/en-sg?category[]=Commerce<br>
 <input id="parameters" type="text" value="" style="width:100%;" /><br><br>
 <label>Select country</label><br>
 <select id="country">
@@ -41,7 +43,7 @@ td{padding:10px;}
 </form>
 </div>
 
-<div id="contentdata" style="width:80%; padding:20px 10%; text-align:center; border:1px solid #CCC;background: #BBF0FF;font-size:14px;">
+<div id="contentdata" style="width:80%; padding:20px 10%; text-align:center; border:1px solid #CCC;background: #BBF0FF;font-size:14px;display:none;position:relative;">
 </div>
 
 </body>
@@ -67,9 +69,10 @@ $(document).ready(function(){
 					}
 				}
 				fulldata += '</table>';
-				$("#contentdata").html(fulldata);
-			  
-			  $( "#contentdata" ).prepend("<h2>Total number of PDP's: "+data["results"]["total"]+"</h2>");
+				$("#contentdata").append(fulldata);
+			  $( "#contentdata" ).prepend('<input type="button" value="Select Table" onclick="SelectContent(\'datatbl\');">'); 
+			  $( "#contentdata" ).prepend("<h2 style='text-align:left;'>Total number of products found: "+data["results"]["total"]+"<br>Total PDP's found: "+i+"</h2>");
+			  $( "#contentdata" ).show();
 			  //alert( "Load was performed." );
 			});
 			return false;
@@ -78,6 +81,39 @@ $(document).ready(function(){
 		}
 	});
 });
+
+function SelectContent (el) {
+	
+var elemToSelect = document.getElementById (el);
+
+        if (window.getSelection) {  // all browsers, except IE before version 9
+            var selection = window.getSelection ();
+            var rangeToSelect = document.createRange ();
+            rangeToSelect.selectNodeContents (elemToSelect);
+
+            selection.removeAllRanges ();
+            selection.addRange (rangeToSelect);
+
+
+
+        }
+
+    else       // Internet Explorer before version 9
+          if (document.body.createTextRange) {    // Internet Explorer
+                var rangeToSelect = document.body.createTextRange ();
+                rangeToSelect.moveToElementText (elemToSelect);
+                rangeToSelect.select ();
+
+        }
+
+  else if (document.createRange && window.getSelection) {         
+          range = document.createRange();             
+          range.selectNodeContents(el);             
+    sel = window.getSelection();     
+                  sel.removeAllRanges();             
+    sel.addRange(range);              
+ }  
+}
 
 </script>
 </html>
