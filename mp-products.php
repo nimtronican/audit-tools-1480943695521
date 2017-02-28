777ab4c03bc25440483bd8585ab26b9c36981fdf
nimtronican <NIMTRONICAN@GMAIL.COM>
2017-02-28 12:12:15 +0530

    BU Categorization & Download option
    Added New columns, Taxonomy, Category & Contact Module Priority & Contact module

--- mp-products.php
+++ mp-products.php
@@ -10,18 +10,20 @@ label{font-weight:bold;}
 select{height:28px;}
 h4{color:#B00002}
 button,input[type="button"]{width:200px;font-size:14px; background:#188500; color:#FFFFFF; padding:10px; margin-top:10px;font-weight:bold; border:none; border:1px solid #00480A;border-radius:3px;cursor:pointer;}
-input[type="button"]{ position:absolute; top:10px; right:60px;background-color:#00828C;}
+input[type="button"]{ float:right;background-color:#00828C;margin-left:10px;}
 table{width:100%;}
+table th{text-align:center;padding:2px 0;}
 table,td,tr{background:#FFFFFF;border-collapse:collapse;text-align:left;}
 td{padding:10px;}
 </style>
 </head>
 <body style="text-align:center;">
-<h1>Find Marketplace PDP list under a category</h1>
-<h4>Note: Only the Search URL's which contains "category[]=" is valid to use</h4>
+<h1>Find Marketplace PDP list for a Country</h1>
+<h4>Enter "?" to list all pages (or) Category Listing URL (or) Search Term</h4>
 <div id="inputdata" style="width:80%; padding:20px 10%; text-align:center; border:1px solid #CCC;background:#FFF9F9;font-size:14px;">
 <form id="urlparameters">
-<label>Enter the Category Search URL like below:<br></label><br>Example: https://www.ibm.com/marketplace/search/sg/en-sg?category[]=Commerce<br>
+<label><strong>Enter the Category Search URL</strong><br></label>Example: https://www.ibm.com/marketplace/search/sg/en-sg?category[]=Commerce<br>(or)<br>
+<strong>Enter the Search Term URL</strong><br></label>Example: https://www.ibm.com/marketplace/search/sg/en-sg?terms=watson<br>
 <input id="parameters" type="text" value="" style="width:100%;" /><br><br>
 <label>Select country</label><br>
 <select id="country">
@@ -35,10 +37,15 @@ td{padding:10px;}
 <option value="en-th">AP-Thailand</option>
 <option value="en-vn">AP-Vietnam</option>
 <option value="en-ph">AP-Phillipines</option>
-<option value="fr-fr">AP-Korea</option>
+<option value="ko-kr">AP-Korea</option>
 <option value="ja-jp">Japan-Japan</option>
 <option value="en-za">MEA-South Africa</option>
 <option value="en-ae">MEA-UAE</option>
+<option value="en-sa">MEA-Saudi Arabia</option>
+<option value="en-eg">MEA-Egypt</option>
+<option value="zh-cn">China-China</option>
+<option value="zh-tw">China-Taiwan</option>
+<option value="es-mx">SA-Mexico</option>
 <option value="fr-fr">Europe-France</option>
 <option value="de-de">Europe-Germany</option>
 <option value="it-it">Europe-Italy</option>
@@ -48,24 +55,30 @@ td{padding:10px;}
 <option value="tr-tr">Europe-Turkey</option>
 <option value="ru-ru">Europe-Russia</option>
 <option value="pl-pl">Europe-France</option>
+<option value="en-uk">Europe-UK</option>
+<option value="en-nl">Europe-Dutch</option>
 <option value="en-us">NA-USA</option>
+<option value="en-ca">NA-Canada(English)</option>
 <option value="fr-ca">NA-Canada(french)</option>
-<option value="zh-cn">China-China</option>
-<option value="zh-tw">China-Taiwan</option>
-<option value="es-mx">SA-Mexico</option>
-<option value="en-uk">UK</option>
-<option value="en-nl">Dutch</option>
+<option value="en-kw">MEA-Kuwait</option>
+<option value="en-bh">MEA-Bahrain</option>
+<option value="en-om">MEA-Oman</option>
+<option value="en-qa">MEA-Qatar</option>
+<option value="en-pk">MEA-Pakistan</option>
+<option value="en-jo">MEA-Jordan</option>
+<option value="en-lb">MEA-Lebanon</option>
 </select><br>
 <button id="findproducts">Find PDP List</button>
 </form>
 </div>
 
-<div id="contentdata" style="width:80%; padding:20px 10%; text-align:center; border:1px solid #CCC;background: #BBF0FF;font-size:14px;display:none;position:relative;">
+<div id="contentdata" style="padding:20px 1%; text-align:center; border:1px solid #CCC; border-top:none;background: #BBF0FF;font-size:14px;display:none;position:relative;">
 </div>
 
 </body>
 <!-- Additional JS Controllers -->
 <script type="text/javascript" src="./js/jquery-3.1.1.min.js"></script>
+<script type="text/javascript" src="./js/jquery.table2excel.js"></script>
 <script type="text/javascript">
 $(document).ready(function(){
 	$("#findproducts").click(function(){
@@ -91,22 +104,46 @@ $(document).ready(function(){
 					$.get( geturl1, function( data ) {
 				  //alert(data["results"]["items"][1]["doc"]["contact"].toSource());
 					var fulldata = '<table id="datatbl" border="1" cellpadding="0" cellspacing="0">';
-					fulldata +='<tr><th>Sno</th><th>Product Name</th><th>Product URL</th><th>Purchasable</th></tr>';
+					fulldata +='<tr><th>Sno</th><th>Product Name</th><th>Product URL</th><th>Taxonomy</th><th>Category</th><th>Contact Module<br>(Priority Code)</th><th>Contact Module Form)</th><th>Purchasable</th></tr>';
 					var purchasable = "No";
+					var cntry_code = cntry.split("-");
+					cntry_code = cntry_code[0]+"_"+cntry_code[1].toUpperCase();
+					//cntry_code = cntry_code[1]+"_"+cntry_code[0];
+					//alert(cntry_code);
 					for(var i=0;i<maincount;i++)
 					{
 						if(typeof data["results"]["items"][i] !== 'undefined'){
 							
 							if(typeof data["results"]["items"][i]["doc"]["commerce"] != 'undefined'){purchasable = "Yes";}
-							fulldata += '<tr><td>'+(i+1)+'</td><td>'+data["results"]["items"][i]["doc"]["name"]+'</td><td>'+data["results"]["items"][i]["doc"]["url"]+'</td><td>'+purchasable+'</td>';
+							fulldata += '<tr><td>'+(i+1)+'</td><td>'+data["results"]["items"][i]["doc"]["name"]+'</td><td>'+data["results"]["items"][i]["doc"]["url"]+'</td>';
+							if(typeof data["results"]["items"][i]["doc"]["taxonomy"]!= 'undefined'){
+								fulldata += '<td>'+data["results"]["items"][i]["doc"]["taxonomy"]+'</td>';
+							}else{
+								fulldata += '<td></td>';
+							}
+							if(typeof data["results"]["items"][i]["doc"]["category"]!= 'undefined'){
+								fulldata += '<td>'+data["results"]["items"][i]["doc"]["category"]+'</td>';
+							}else{
+								fulldata += '<td></td>';
+							}
+							if(typeof data["results"]["items"][i]["doc"]["contact"]!= 'undefined'){
+								fulldata += '<td>'+data["results"]["items"][i]["doc"]["contact"][cntry_code]["priority"]+'</td>';
+								fulldata += '<td>'+data["results"]["items"][i]["doc"]["contact"][cntry_code]["contact-web-form"]+'</td>';
+							}else{
+								fulldata += '<td></td><td></td>';
+							}
+							fulldata += '<td>'+purchasable+'</td>';
+							
 							purchasable = "No";
+							
 						}
 					}
 					fulldata += '</table>';
 				  $("#contentdata").html("");
 				  $("#contentdata").append(fulldata);
-				  $( "#contentdata" ).prepend('<input type="button" value="Select Table" onclick="SelectContent(\'datatbl\');">'); 
-				  $( "#contentdata" ).prepend("<h2 style='text-align:left;'>Total number of products found: "+maincount+"</h2>");
+				  $( "#contentdata" ).prepend('<input type="button" id="selecttbl" value="Select Table" onclick="SelectContent(\'datatbl\');" />'); 
+				  $( "#contentdata" ).prepend('<input type="button" id="download" value="Download Table as Excel" onclick="downloadContent(\'datatbl\',\''+cntry_code+'\');">'); 
+				  $( "#contentdata" ).prepend("<h2 style='text-align:left;float:left;'>Total number of products found: "+maincount+"</h2>");
 				});
 			});
 			return false;
@@ -116,8 +153,17 @@ $(document).ready(function(){
 		}
 		
 	});
+	
 });
-
+function downloadContent(el,ccd) {
+		//alert(el+ccd);
+	$("#"+el).table2excel({
+		 // exclude CSS class
+	    exclude: ".noExl",
+	    name: "mpurl_"+ccd,
+	    filename: Math.floor(Math.random()*100000)+"_"+ccd.toLowerCase()
+	  }); 
+}
 function SelectContent (el) {
 	
 var elemToSelect = document.getElementById (el);
@@ -150,6 +196,5 @@ var elemToSelect = document.getElementById (el);
     sel.addRange(range);              
  }  
 }
-
 </script>
 </html>
