beb646953cde057113d7c28c396a04124dc20972
nimtronican <NIMTRONICAN@GMAIL.COM>
2018-04-18 13:52:24 +0000

    API test files for IBM NOW

---
+++ api-tools/api-check.js
@@ -0,0 +1,216 @@
+var base_url = "http://localhost/gateway_main.php";
+var base_url = "ibmnowtestapi.mybluemix.net/gateway_main.php";
+ibmbox = {};
+ibmbox.util = {};
+ibmbox.util.doFeed = {
+	init: function () {
+		$('#result').val("Results will be displayed here...");
+	},
+	doAPI001: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'GET_POINT_COUNT', 'event_id':32,'customer_id':386,"customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199"};
+		var url = base_url+"/api/event/get_customer_event_point.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	/*doAPI002: function () {
+		var param = {'api_key':'API_KEY', 'event_code':'watsonsummit2017','customer_id':'386'};
+		var url = base_url+"/api/event/get_customer_event_record_count.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	doAPI003: function () {
+		var param = {'api_key':'API_KEY', 'event_code':'watsonsummit2017','customer_id':'342','exhibition_code':'47'};
+		var url = base_url+"/api/event/get_customer_exhibition_application.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},*/
+	doAPI004: function () {
+		var param = {'api_key':'API_KEY','api_name':'GET_SESSION_APPLICATION', "customer_id":"342","customer_authkey":"1d8b4257910493fddff2211575d7fa2f","event_id":32};
+		var url = base_url+"/api/event/get_customer_program_session_application.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	doAPI005: function () {
+		var param = {'api_key':'API_KEY','api_name':'CREATE_EVENT_CHECKIN', 'event_id':32,"customer_authkey":"1d8b4257910493fddff2211575d7fa2f","customer_id":342,"device_type":'APP'};
+		var url = base_url+"/api/event/create_customer_event_record.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	doAPI006: function () {
+
+		var param = {'api_key':'API_KEY', 'api_name':'CREATE_POINT_ADJUST','event_id':32,'customer_id':386,"customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199",'point':10, "point_type":"LIVE_POLL",'record_date':'2018-02-18'};
+		var url = base_url+"/api/event/create_customer_event_point.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	doAPI007: function () {
+		var jsobj = new Object();
+		jsobj.exhibition_id_list = "151,152,153,154,155,156,157,158,159";
+		/*jsobj.exhibition_id_list = new Array('44','55');*/
+		var param = {'api_key':'API_KEY','api_name':'CREATE_EXHIBITION_CHECKIN','event_id':32,"customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199",'customer_id':386,'exhibition_id_list':JSON.stringify(jsobj)};
+		var url = base_url+"/api/event/create_customer_exhibition_record.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	doAPI008: function () {
+		var jsobj = new Object();
+		jsobj.session_id_list = "277,278,294,315,396";
+		var param = {'api_key':'API_KEY', 'api_name':'CREATE_SESSION_CHECKIN','event_id':32,"customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199",'customer_id':386,'session_id_list':JSON.stringify(jsobj)};
+		var url = base_url+"/api/event/create_customer_program_session_record.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	
+	doAPI009: function () {
+		var param = {'api_key':'API_KEY','api_name':'GET_USER_LOGIN', 'event_id':32,'login_email':'nimalakanth@in.ibm.com','login_pass':'shinsuke12haneda'};
+		var url = base_url+"/api/event/check_user_login.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	doAPI010: function () {
+		var param = {'api_key':'API_KEY','api_name':'GET_PROFILE_ALL','event_id':32,"customer_id":"0","customer_authkey":"hsjfhjads6w871687hjkdsf"};
+		var url = base_url+"/api/event/get_customer_profile.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	/* doAPI011: function () {
+		var param = {'api_key':'API_KEY','api_name':'GET_PROFILE_ALL','event_id':32,'customer_id':'386',"customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199","login_email":"nimalakanth@in.ibm.com", "url":"https://www.ibm.com/in-en/"};
+		var url = base_url+"/api/event/customer_auto_login.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	}, */
+	doAPI012: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'GET_SESSION_EXHIBITION','event_id':32,"customer_id":"386","customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199"};
+		var url = base_url+"/api/event/get_event.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	/* doAPI013: function () {
+
+		var param = {'api_key':'API_KEY', 'api_name':'','event_code':'watsonsummit2017',"customer_id":"386","customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199","event_id":32,'program_session_code':'M2-2'};
+		var url = base_url+"/api/event/create_customer_program_session_application.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	}, */
+	doAPI014: function () {
+
+		var param = {'api_key':'API_KEY', 'api_name':'CREATE_EXHIBITION_ANALYZED','event_id':32,'customer_id':'386','customer_authkey':'e106ce3ed6c55b6588fb6df9d7c02199'};
+		var url = base_url+"/api/event/create_analyze_exhibition.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	doAPI015: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'GET_EXHIBITION_RECOMMEND','event_id':32,'customer_id':'386','customer_authkey':'e106ce3ed6c55b6588fb6df9d7c02199'};
+		var url = base_url+"/api/event/get_customer_event_recomended_exhibition.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	/* doAPI016: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'','event_code':'watsonsummit2017','customer_id':'342','device_id':'9847455','device_type':'APP','customer_authkey':'hsjfhjads6w871687hjkdsf'};
+		var url = base_url+"/api/event/create_customer_event_change_device.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	}, */
+	doAPI017: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'CREATE_SESSION_CHANGES','event_id':32,'customer_id':'386','session_id':'294','customer_authkey':'e106ce3ed6c55b6588fb6df9d7c02199'};
+		var url = base_url+"/api/event/create_program_session_change.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+	doAPI018: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'CREATE_EXHIBITION_CHANGES','event_id':32,'customer_id':'386','exhibition_id':'154','customer_authkey':'e106ce3ed6c55b6588fb6df9d7c02199'};
+		var url = base_url+"/api/event/create_program_exhibition_change.php";
+		var url = base_url ;
+		this.getJSON(param,url);
+	},
+
+	/* doAPI021: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'','event_id':32,"customer_authkey":"1d8b4257910493fddff2211575d7fa2f",'customer_id':342,'device_type':'APP',"event_checkedin":'Yes'};
+		var url = base_url+"/api/event/create_event_checkin_pin.php" ;
+		var url = base_url ;
+		this.getJSON(param,url);
+	}, */ 
+
+	doAPI022: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'GET_PROFILE','event_id':32,"customer_id":"386","customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199"};
+		var url = base_url+"/api/event/get_customer_profile.php";
+		this.getJSON(param,url);
+	}, 
+
+	doAPI023: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'GET_SESSION_EXHIBITION','event_id':32,"customer_id":"386","customer_authkey":"e106ce3ed6c55b6588fb6df9d7c02199"};
+		var url = base_url+"/api/event/get_event.php";
+		this.getJSON(param,url);
+	}, 
+	doAPI024: function () {
+		var param = {'api_key':'API_KEY', 'api_name':'SEND_EMAIL','event_id':32,'customer_id':342};
+		var url = base_url+"/api/event/create_customer_sendmail.php" ;
+		this.getJSON(param,url);
+	}, 
+	getJSON: function (param,url) {
+		$.ajax({
+			type: "POST",
+			data: param,
+			dataType:'json',
+			url: url,
+			timeout:10000,
+			cache: false,
+			success: function(res){
+				console.log('Success:: geJSON:');
+				ibmbox.util.doFeed.proceed(res);
+			},
+			error:function(res){
+				console.log('Error:: cannot getJSON');
+				console.log(res);
+			}
+		});
+	},
+	proceed: function (data) {
+		console.log('Success:: proceed:'+JSON.stringify(data));
+		$('#result').val(JSON.stringify(data));
+	}
+}
+
+ibmbox.util.require = {
+	loadlib: function (src, callback) {
+		var done = false;
+		var head = document.getElementsByTagName('head')[0];
+		var script = document.createElement('script');
+		script.language='javascript';
+		script.type='text/javascript';
+		script.charset='utf-8';
+		script.src = src;
+		head.appendChild(script);
+		script.onload = script.onreadystatechange = function() {
+			if ( !done && (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") ) {
+				done = true;
+				callback();
+				script.onload = script.onreadystatechange = null;
+				if ( head && script.parentNode ) {
+					head.removeChild( script );
+				}
+			}
+		}
+	}
+}
+
+if(typeof(jQuery) != 'function') {
+	var jquery_url = "http://code.jquery.com/jquery-1.11.0.min.js";
+	ibmbox.util.require.loadlib(jquery_url, function() {
+		console.log("ibmbox.util.require.loadlib: "+jquery_url);
+		if(typeof($) != 'function') {
+			$ = jQuery;
+		}
+		$(function(){
+			ibmbox.util.doFeed.init();
+		});
+	});
+} else {
+	if(typeof($) != 'function') {
+		$ = jQuery;
+	}
+	$(function(){
+		ibmbox.util.doFeed.init();
+	});
+}
+
---
+++ api-tools/api-check.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja-JP">
+<head>
+	<meta charset="utf-8" />
+	<script src="./api-check.js"></script>
+</head>
+</head>
+<body>
+<button onclick="ibmbox.util.doFeed.doAPI009();" style="background:#FF0">2-1.check_user_login</button>
+<button onclick="ibmbox.util.doFeed.doAPI005();" style="background:#FF0">2-2.create_customer_event_record</button>
+<button onclick="ibmbox.util.doFeed.doAPI010();" style="background:#FF0">2-3.get_customer_profile</button>
+<button onclick="ibmbox.util.doFeed.doAPI022();" style="background:#FF0">2-3.get_user_allprofile_pinmicro</button>
+<button onclick="ibmbox.util.doFeed.doAPI004();" style="background:#FF0">2-4.get_customer_program_session_application</button>
+<br />
+<button onclick="ibmbox.util.doFeed.doAPI012();" style="background:#FF0">2-5.get_exhibitionsessionList</button>
+<button onclick="ibmbox.util.doFeed.doAPI023();" style="background:#FF0">2-5.get_exhibitionsessionList_pinmicro</button>
+<button onclick="ibmbox.util.doFeed.doAPI007();" style="background:#FF0">2-6.create_customer_exhibition_record</button>
+<button onclick="ibmbox.util.doFeed.doAPI008();" style="background:#FF0">2-6-1.create_customer_program_session_record</button>
+<br />
+<button onclick="ibmbox.util.doFeed.doAPI001();" style="background:#FF0">2-7.get_customer_event_point</button>
+<button onclick="ibmbox.util.doFeed.doAPI006();" style="background:#FF0">2-8.create_customer_event_point</button>
+<button onclick="ibmbox.util.doFeed.doAPI014();" style="background:#FF0">2-9.create_analyze_exhibition</button>
+<button onclick="ibmbox.util.doFeed.doAPI015();" style="background:#FF0">2-10.get_analyze_exhibition</button>
+<br />
+<button onclick="ibmbox.util.doFeed.doAPI017();" style="background:#FF0">2-12.create_changeSessionAvailability</button>
+<button onclick="ibmbox.util.doFeed.doAPI018();" style="background:#FF0">2-13.create_changeExhibitionAvailability</button>
+<button onclick="ibmbox.util.doFeed.doAPI011();" style="background:#FF0">4-1.customer_auto_login</button>
+<br />
+<!--<button onclick="ibmbox.util.doFeed.doAPI002();">get_customer_event_record_count</button>
+<button onclick="ibmbox.util.doFeed.doAPI003();">get_customer_exhibition_application</button>
+<button onclick="ibmbox.util.doFeed.doAPI016();">cretae_change_device_type</button>
+<button onclick="ibmbox.util.doFeed.doAPI013();">create_customer_program_session_application</button>
+<button onclick="ibmbox.util.doFeed.doAPI021();">Create_EventCheckinPin</button>-->
+
+<button onclick="ibmbox.util.doFeed.doAPI024();" style="background:#FF0">Send_mail</button>
+<br />
+<textarea id="result" rows="20" cols="100"></textarea>
+</body>
+</html>
